1286514ad46477f70d5ca03b30bfd561e6f729da
richie <rickromero99@gmail.com>
2019-03-02 11:38:01 -0800

    homework

---
+++ assets/javascript/question.js
@@ -0,0 +1,80 @@
+var questions = [{
+         "question": "What Year Did Taxi Drive Come Out?",
+         "option1":"1981",
+         "option2":"1984",
+         "option3":"1976",
+         "option4": "1966",
+         "answer": "3"
+    //first question is not loading
+}, {
+        "question": "What is  Taxi Driver name?",
+        "option1": "MR.Cooper",
+        "option2": "Travis Bickle",
+        "option3": "Christopher Moltisanti",
+        "option4": "Frank Rizzo",
+        "answer": "2"
+    
+}, {
+        "question": "What dose Taxi Driver like to eat?",
+        "option1": "Steak",
+        "option2": "Donuts",
+        "option3":  "Apple Pie with a slice Of yellow cheese",
+        "option4": "Fried Iceream And Anchovies",
+        "answer": "3"
+},{
+        "question": "What city dose it take place in?",
+        "option1": "New York City",
+        "option2": "NEW Jersey",
+        "option3": "Barcelona",
+        "option4": "Chicago",
+        "answer":  "1"
+
+},{
+        "question": "What can Taxi Driver use a 45 for?",
+        "option1":"kill an Elephant",
+        "option2":"To Shoot",
+        "option3":"To Show Off",
+        "option4":"Join The CIA",
+        "answer": "1"
+
+}, {
+        "question": "What is Taxi Driver real name?",
+        "option1": "Nicky Santoro",
+        "option2": "Robert De niro",
+        "option3": "Tony Soprrano",
+        "option4": "Billy Bats",
+        "answer": "2"
+
+}, {
+        "question": "What is Taxi Driver favoreit movie?",
+        "option1": "Taxi Driver",
+        "option2": "Saturday Night Fever",
+        "option3": "Rocky",
+        "option4": "Pornos",
+        "answer": "4"
+},{
+        "question": "When can he work?",
+        "option1": "Only Nights",
+        "option2": "Any Time Any Where",
+        "option3": "Only On Weekends",
+        "option4": "Hollidays Only",
+        "answer":  "2"
+}, {
+            "question": "What is Taxi Driver favoreit hair cut?",
+            "option1": "Jerry Curl",
+            "option2": "Hi and Tight",
+            "option3": "Mohawk",
+            "option4": "Slick Back",
+            "answer": "3"
+},{
+            "question": "Whats is favorite candy?",
+            "option1": "Jujubess",
+            "option2": "Chocolate",
+            "option3": "Jolly Ranchers",
+            "option4": "Lemon Sherbert",
+            "answer":  "1"
+
+
+
+}
+]
